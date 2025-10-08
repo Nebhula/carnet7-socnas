6d752f22ced9df25f730eac30e297f55bfc27eff
Nebhula <josema.mera37@gmail.com>
2025-10-08 13:12:24 +0200

    Update checkPassword.js

--- netlify/functions/checkPassword.js
+++ netlify/functions/checkPassword.js
@@ -1,1 +1,15 @@
+exports.handler = async (event) => {
+  const { password } = JSON.parse(event.body);
 
+  if (password === '123') {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true, dni: '48538463E' }),
+    };
+  }
+
+  return {
+    statusCode: 401,
+    body: JSON.stringify({ success: false, message: 'Contraseña incorrecta' }),
+  };
+};
